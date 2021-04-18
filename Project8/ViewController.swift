751cbbfe516319817887ef87981bdff8a559dd1f
Paul Richardson <developer@photographerafoot.com>
2021-04-19 08:34:56 +1000

    CHG: add properties for interface elements

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var cluesLabel: UILabel!
+	var answersLabel: UILabel!
+	var currentAnswer: UITextField!
+	var scoreLabel: UILabel!
+	var letterButtons = [UIButton]()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
