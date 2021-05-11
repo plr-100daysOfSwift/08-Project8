7fa6a7136c2f365c1ebe81ff35daec788c90eaac
Paul Richardson <developer@photographerafoot.com>
2021-05-12 07:57:36 +1000

    CHG: animate alpha when letter tapped

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -139,7 +139,14 @@ class ViewController: UIViewController {
 		guard let buttonTitle = sender.titleLabel?.text else { return }
 		currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
 		activatedButtons.append(sender)
-		sender.isHidden = true
+
+		UIView.animate(withDuration: 0.5, delay: 0, options: []) {
+			sender.alpha = 0
+		} completion: { finished in
+			sender.isHidden = true
+			sender.alpha = 1
+		}
+
 	}
 
 	@objc func clearTapped(_ sender: UIButton) {
